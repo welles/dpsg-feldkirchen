357e842dc94258d52f65b4b092a154ecf778a827
Nico Welles <nico@welles.io>
2020-08-25 19:04:38 +0200

    Add basic page structure

---
+++ StammPhoenix.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StammPhoenix.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return this.View();
+        }
+    }
+}
