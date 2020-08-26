31971d34256306cc047999bb1d60500708711e46
Nico Welles <nico@welles.io>
2020-08-26 14:58:10 +0200

    Add index page with content

--- StammPhoenix.Web/Controllers/HomeController.cs
+++ StammPhoenix.Web/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace StammPhoenix.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IWebHostEnvironment _environment;
+
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var path = System.IO.Path.Combine(_environment.WebRootPath, "html/index.html");
+
+            var content = System.IO.File.ReadAllText(path);
+
+            return this.View("Index", content);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
